d0c333e1f4dac7f55e9cfb4570c947386978f815
1474net <gentostage@gmail.com>
2020-05-08 15:01:32 +0900

    Create model, config admin panel, ckeditor

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,10 @@ from config import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('api/v1/', include('movies.urls'))
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
